c2f8b2e8c7bb61d58fb614046786fe87efb95d27
sravanidudekula <105922340+sravanidudekula@users.noreply.github.com>
2022-05-30 20:21:01 +0800

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,29 @@
+let wordCloud = ["Hello", "hii", "how", "what", "you", "yourself", "name", "victory", "food", "lovely", "beautiful", "written", "where", "who", "awesome"];
+let wordsContainer = document.getElementById("wordsContainer");
+let userInput = document.getElementById("userInput");
+let errorMsgEl = document.getElementById("errorMsg");
+let errorMsg = "Please enter a word";
+
+function createAndAddWordToWordCloud(word) {
+    let randomFontSize = Math.ceil(Math.random() * 40) + "px";
+    let wordEl = document.createElement("span");
+    wordEl.textContent = word;
+    wordEl.style.fontSize = randomFontSize;
+    wordEl.classList.add("m-3");
+    wordsContainer.appendChild(wordEl);
+
+}
+for (let word of wordCloud) {
+    createAndAddWordToWordCloud(word);
+}
+
+function addRandomNumber() {
+    let userEnteredWord = userInput.value;
+    if (userEnteredWord !== "") {
+        userInput.value = "";
+        errorMsgEl.textContent = "";
+        createAndAddWordToWordCloud(userEnteredWord)
+    } else {
+        errorMsgEl.textContent = errorMsg;
+    }
+}
